5f13567780420d256d213c810583ea0fe5afe1d8
符玄 <3072252442@qq.com>
2024-09-15 18:02:17 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 from Fuxuan import Divination
 from Fuxuan import Interpretation
 
+# build exe file command:
+# pyinstaller -F --version-file version.txt main.py -i F:\python_play\pixiv\dist\符玄表情包\img5.gif
+
 if __name__ == '__main__':
     divination = Divination()
     interpretation = Interpretation()
-    hexagram , result = divination.run("我明天的运势如何?")
+    question = input("请输入你要占卜的问题：")
+    hexagram , result = divination.run(question)
     interpretation.interpret(hexagram[0], hexagram[1], hexagram[2], result, out_to_file=True)
